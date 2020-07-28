1901c087b3e9ae88be764299c452dcb66eeb43a6
johnpolacek <johnpolacek@gmail.com>
2020-07-28 10:52:11 -0500

    test redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 })
 module.exports = withMDX({
+  async redirects() {
+    return [
+      {
+        source: '/test',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
   pageExtensions: ["js", "jsx", "md", "mdx"],
   webpack: (config, { isServer }) => {
     // Fixes npm packages (mdx) that depend on `fs` module
