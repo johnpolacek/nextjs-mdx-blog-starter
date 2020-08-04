fac99857510332a1d2fde34385ef1a9b41f11308
johnpolacek <johnpolacek@gmail.com>
2020-08-04 14:24:59 -0500

    update nav

--- src/ui/Nav.js
+++ src/ui/Nav.js
@@ -1,14 +1,22 @@
+import { useRouter } from "next/router"
 import { Box } from "theme-ui"
 import NavLink from "./NavLink"
 
-const Nav = (props) => (
-  <Box
-    as="nav"
-    sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
-  >
-    <NavLink href="/">Home</NavLink>
-    <NavLink href="/about">About</NavLink>
-  </Box>
-)
+const Nav = (props) => {
+  const router = useRouter()
+  return (
+    <Box
+      as="nav"
+      sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+    >
+      <NavLink href="/" active={router.pathname === "/" || router.pathname.includes("/posts")}>
+        Blog
+      </NavLink>
+      <NavLink href="/about" active={router.pathname === "/about"}>
+        About
+      </NavLink>
+    </Box>
+  )
+}
 
 export default Nav
--- src/ui/NavLink.js
+++ src/ui/NavLink.js
@@ -1,32 +1,23 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import PropTypes from "prop-types"
 import Link from "next/link"
-import { useRouter } from "next/router"
 
-const NavLink = (props) => {
-  const router = useRouter()
-  return (
-    <Link href={props.href}>
-      <a
-        sx={{
-          py: [2, 3],
-          px: 3,
-          fontSize: 3,
-          fontWeight: 200,
-          display: "inline-block",
-          textDecoration: "none",
-          borderBottom: "1px solid",
-          borderColor: router.pathname === props.href ? "primary" : "white",
-        }}
-        {...props}
-      />
-    </Link>
-  )
-}
-
-NavLink.propTypes = {
-  href: PropTypes.string.isRequired,
-}
+const NavLink = ({href, active, children}) => (
+  <Link href={href}>
+    <a
+      sx={{
+        py: [2, 3],
+        px: 3,
+        fontSize: 3,
+        fontWeight: 200,
+        display: "inline-block",
+        textDecoration: "none",
+        borderBottom: "1px solid",
+        borderColor: active ? "primary" : "white",
+        cursor: "pointer"
+      }}
+      >{children}</a>
+  </Link>
+)
 
 export default NavLink
