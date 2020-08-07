565b412787c7e640aec89670e3bdf4ea10804ade
johnpolacek <johnpolacek@gmail.com>
2020-08-07 08:01:56 -0500

    make about page a named export

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import Wrapper from "../src/layout/Wrapper"
 import About from "../src/views/About"
 import config from "../blog.config.js"
 
-export default () => (
+const AboutPage = () => (
   <Wrapper
     url="/about"
     title={config.title + " | About"}
@@ -13,3 +13,5 @@ export default () => (
     <About />
   </Wrapper>
 )
+
+export default AboutPage
