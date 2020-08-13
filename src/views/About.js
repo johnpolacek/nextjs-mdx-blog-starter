4d7fd36d1b5f5b2199c9fb631ad7197bd058e941
johnpolacek <johnpolacek@gmail.com>
2020-08-13 07:00:05 -0500

    update about page

--- src/views/About.js
+++ src/views/About.js
@@ -1,14 +1,10 @@
 import { MDXProvider } from "@mdx-js/react"
 import MarkdownContent from "../mdx/about.mdx"
-import { Box } from "theme-ui"
 import Container from "../ui/Container"
 
 const About = (props) => {
-  const components = {
-    Box: (props) => <Box {...props} />,
-  }
   return (
-    <MDXProvider components={components}>
+    <MDXProvider>
       <Container>
         <MarkdownContent />
       </Container>
