22fb4b8fa9542e3472058feb370cfb58e109b061
johnpolacek <johnpolacek@gmail.com>
2020-08-14 13:29:12 -0500

    update shareImage url

--- blog.config.js
+++ blog.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "Next.js MDX Blog Starter for building blogs with Next.js and MDX, including Theme UI Component Design System, Vercel Deployment and more.",
   author: "John Polacek",
   analytics: "", // put your google analytics code here to add tracking
-  shareImage: "https://nextjs-mdx-blog-starter.now.sh/img/screenshot.png",
+  shareImage: "https://nextjs-mdx-blog-starter.vercel.app/img/screenshot.png",
   shareImageAlt: "Next.js MDX Blog Starter Screenshot",
   postsPerPage: 8,
   showDate: true, // if you would like the date to appear on the blog posts
