602494e8f9e0ef8c47198f3615663b69afef7848
johnpolacek <johnpolacek@gmail.com>
2020-12-27 08:55:19 -0600

    update ButtonLink component

--- src/ui/ButtonLink.js
+++ src/ui/ButtonLink.js
@@ -1,32 +1,49 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import React from "react"
 import PropTypes from "prop-types"
-import Link as NextLink from "next/link"
-import { Link } from "theme-ui"
+import Link from "next/link"
 
-const ButtonLink = (props) => (
-  <NextLink as={props.as || props.href} href={props.href}>
-    <Link
-      disabled={props.disabled}
-      sx={{
-        textDecoration: "none",
-        fontSize: 3,
-        px: 3,
-        py: 2,
-        bg: "black",
-        color: "#fff",
-        boxShadow: "none",
-        borderRadius: "4px",
-        border: "none",
-        cursor: "pointer",
-        display: "inline-block",
-      }}
-      {...props}
-    />
-  </NextLink>
-)
+const A = React.forwardRef(({ onClick, href, styles, children }, ref) => {
+  return (
+    <a href={href} sx={styles} ref={ref}>
+      {children}
+    </a>
+  )
+})
 
-ButtonLink.propTypes = {
-  href: PropTypes.string.isRequired,
-  as: PropTypes.string,
+const ButtonLink = (props) => {
+  let py = 2
+  if (typeof props.fontSize === "number") {
+    py = props.fontSize - 1
+  } else if (props.fontSize.length > 1) {
+    py = props.fontSize.map((n) => n - 1)
+  }
+
+  const styles = {
+    textDecoration: "none",
+    fontWeight: props.fontWeight || "normal",
+    fontSize: props.fontSize || 3,
+    px: props.fontSize || 3,
+    py,
+    bg: props.bg || "primary",
+    color: props.bg === "white" ? "primary" : "#fff",
+    boxShadow: "none",
+    borderRadius: "8px",
+    border: "none",
+    cursor: "pointer",
+    display: "inline-block",
+  }
+  return props.href.includes("http") ? (
+    <A href={props.href} styles={styles}>
+      {props.children}
+    </A>
+  ) : (
+    <Link href={props.href} passHref>
+      <A styles={styles}>{props.children}</A>
+    </Link>
+  )
 }
 
 export default ButtonLink
