b949899f1446b530cc5343e789cabc3f310fb8a6
Erik Stockmeier <erik.d.stock@gmail.com>
2021-02-07 07:50:36 -0500

    types cleanup, remove unused ButtonLink

--- src/ui/Link.tsx
+++ src/ui/Link.tsx
@@ -3,8 +3,20 @@
 import { jsx } from "theme-ui"
 import NextLink from "next/link"
 
-export const Link: React.FC<{ href: string }> = ({ href, children }) => {
-  return (
+interface Props {
+  href: string
+  external: boolean
+}
+
+/**
+ * A general link for the
+ */
+export const Link: React.FC<Props> = ({ href, external = false, children }) => {
+  return external ? (
+    <a target="_blank" sx={{ variant: "styles.a" }}>
+      {children}
+    </a>
+  ) : (
     <NextLink passHref href={href}>
       <a sx={{ variant: "styles.a" }}>{children}</a>
     </NextLink>
